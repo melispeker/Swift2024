98aaa9a4fb757d3aefc1c7a4f2cf2381db035059
Melis Peker <melis.peker@hisarschool.k12.tr>
2024-09-18 19:25:56 +0300

    Update Contents.swift

--- Fonksiyonlar.playground/Contents.swift
+++ Fonksiyonlar.playground/Contents.swift
@@ -6,3 +6,13 @@ func sayilariTopla(a: Int, b: Int) -> Int {
 }
 
 let sonuc = sayilariTopla(a: 5, b:)
+
+
+
+func dikdortgenAlani(uzunluk: Double, genislik: Double) -> Double {                
+    return uzunluk * genislik                                                     
+}
+
+// Örnek kullanım
+let alan = dikdortgenAlani(uzunluk: 10.0, genislik: 5.0)                            
+print("Dikdörtgenin alanı: \(alan)")                                               
